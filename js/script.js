d5ef5bdeed9dee14c8b6241ddb1c87e053fc4879
Mykhailo Zanizdra <yamixa.gz@gmail.com>
2025-05-03 19:17:42 +0300

    feat: Made modal responsive.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const burgerMenu = document.getElementById("burger-menu");
+    const burgerMenu = document.querySelector(".burger-icon");
 
     burgerMenu.addEventListener("click", function() {
         const menu = document.querySelector(".mobile-menu");
@@ -10,5 +10,16 @@ document.addEventListener("DOMContentLoaded", function() {
     closeButton.addEventListener("click", function() {
         const menu = document.querySelector(".mobile-menu");
         menu.classList.remove("is-open");
+    });
+
+    const modalOpenButton = document.querySelector(".hero-button");
+    modalOpenButton.addEventListener("click", function() {
+        const modal = document.querySelector(".hero-modal-overlay");
+        modal.classList.add("is-open");
+    });
+    const modalCloseButton = document.querySelector(".hero-modal-close-button");
+    modalCloseButton.addEventListener("click", function() {
+        const modal = document.querySelector(".hero-modal-overlay");
+        modal.classList.remove("is-open");
     });
 });
